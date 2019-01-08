ea9a33e2c37e7a2aeb29ff16483455619873f231
Luke Granger <grangerl330@gmail.com>
2019-01-08 21:09:29 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,16 @@
-def reformat_languages(languages)
-  # your code here
-  new_hash = {}
 
-  languages.each do |styles, language_name|
-    language_name.each do |name, type|
+
+def reformat_languages(languages)
+  new_hash = {}
+  
+  languages.each do |styles, language_names|
+    language_names.each do |name, language_type|
       if new_hash.has_key?(name)
-        new_hash[name][:style] << styles
+        new_hash[name][:stlye] << styles 
       else
-        new_hash[name] = type
-        new_hash[name][:style] = [styles]
-      end
+        new_hash[name] = language_type
+        new_hash[name][:stlye] = [styles]
+      end 
     end
   end
   new_hash
