6d32eb86b13e540e11600a5a49912ec2c84094ac
Luke Granger <grangerl330@gmail.com>
2019-01-08 20:52:45 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@
 
 def reformat_languages(languages)
   new_hash = {}
-  languages.each do |style, data|
+  languages.each do |style, languages|
     data.each do |language, language_type|
       new_hash[language] = {type: "#{language_type}"}
       new_hash[:style] = [] << style
